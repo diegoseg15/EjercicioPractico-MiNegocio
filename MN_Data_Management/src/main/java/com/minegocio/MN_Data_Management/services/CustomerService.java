e829c6cd50534403486a72880da5522797e5c10a
diegoseg_15 <diegoferv@gmail.com>
2025-09-27 16:12:36 -0500

    :sparkles: Implementar lógica en CustomerService con métodos de acceso y guardado

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/services/CustomerService.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/services/CustomerService.java
@@ -2,10 +2,40 @@ package com.minegocio.MN_Data_Management.services;
 
 import org.springframework.stereotype.Service;
 
+import com.minegocio.MN_Data_Management.domain.Customer;
+import com.minegocio.MN_Data_Management.repositories.AddressRepository;
+import com.minegocio.MN_Data_Management.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
-    
+
+    private final CustomerRepository customerRepository;
+
+    public Flux<Customer> getAll() {
+        return customerRepository.findAll();
+    }
+
+    public Mono<Customer> getByIdentification(String identification) {
+        return customerRepository.findByIdentification(identification).next();
+    }
+
+    public Flux<Customer> getCustomerByName(String Name) {
+        return customerRepository.searchByFullName(Name);
+    }
+
+    public Mono<Customer> saveCustomer(Customer c) {
+        return customerRepository.save(c)
+                .flatMap(customer -> {
+                    return customerRepository.save(customer);
+                });
+    }
+
+    // public Mono<Address> update(String identification, Customer c){
+    //     return customerRepository.findById(id)
+            
+    // }
 }
