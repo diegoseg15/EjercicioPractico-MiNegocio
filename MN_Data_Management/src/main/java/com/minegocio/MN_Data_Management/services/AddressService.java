10d3996b0a3453695026814d011b21b0cfff73f0
diegoseg_15 <diegoferv@gmail.com>
2025-09-27 16:17:02 -0500

    :sparkles: Implementar AddressService con método saveAddress

---
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/services/AddressService.java
@@ -0,0 +1,18 @@
+package com.minegocio.MN_Data_Management.services;
+
+import org.springframework.stereotype.Service;
+
+import com.minegocio.MN_Data_Management.domain.Address;
+import com.minegocio.MN_Data_Management.repositories.AddressRepository;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class AddressService {
+
+    
+
+    
+}
