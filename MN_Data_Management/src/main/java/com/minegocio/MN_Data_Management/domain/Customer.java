63cda942998247ca566d0d3ff6c1c7a2f76a7b7e
diegoseg_15 <diegoferv@gmail.com>
2025-09-27 16:14:54 -0500

    :necktie: Añadir campos companyId, createdAt y updatedAt en Customer

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/domain/Customer.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/domain/Customer.java
@@ -1,77 +1,104 @@
 package com.minegocio.MN_Data_Management.domain;
 
-import org.springframework.data.annotation.Id;
+import java.time.Instant;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("customers")
 public class Customer {
-    
+
     @Id
-    private String id;
+    private Long id;
+
+    @Column("company_id")
+    private Long companyId;
 
     private String identification;
+
+    @Column("identification_type")
     private String identification_Type;
     private String name;
     private String lastname;
     private String email;
     private String phone;
 
-    public Customer(){}
+    @Column("created_at")
+    private Instant createdAt;
 
-    public Customer(String identification, String identification_Type, String name, String lastname, String email, String phone){
+    @Column("updated_at")
+    private Instant updatedAt;
+
+    public Customer() {
+    }
+
+    public Customer(String identification, String identification_Type, String name, String lastname, String email,
+            String phone, Long companyId) {
         this.identification = identification;
         this.identification_Type = identification_Type;
         this.name = name;
         this.lastname = lastname;
         this.email = email;
         this.phone = phone;
+        this.companyId = companyId;
     }
 
-    //Getters y Setters
+    // Getters y Setters
 
-    public String getIdentification(){
+    public String getIdentification() {
         return identification;
     }
 
-    public void setIdentificacion(String identification){
+    public void setIdentificacion(String identification) {
         this.identification = identification;
     }
 
-    public String getIdentificationType(){
+    public String getIdentificationType() {
         return identification_Type;
     }
 
-    public void setIdentificacionType(String identification_Type){
+    public void setIdentificacionType(String identification_Type) {
         this.identification_Type = identification_Type;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getLastname(){
+    public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname){
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPhone(){
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone){
+    public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
     }
 }
