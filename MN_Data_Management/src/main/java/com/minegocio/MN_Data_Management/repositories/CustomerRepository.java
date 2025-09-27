e5dfb88c328cbdaf5221189ec8c4c09198b722b7
diegoseg_15 <diegoferv@gmail.com>
2025-09-27 16:11:30 -0500

    :sparkles: Agregar queries personalizados en CustomerRepository

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/CustomerRepository.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/CustomerRepository.java
@@ -1,12 +1,28 @@
 package com.minegocio.MN_Data_Management.repositories;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import com.minegocio.MN_Data_Management.domain.Customer;
 
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
-  
+  @Query("SELECT c.* FROM customers c")
+  Flux<Customer> findAllProjected(); 
+
+  @Query("SELECT c.* FROM customers c WHERE c.identification = :identification")
+  Flux<Customer> findByIdentification(String identification);
+
+  @Query("SELECT c.* FROM customers c WHERE c.company_id = :companyId")
+  Flux<Customer> findByCompanyId(Long companyId);
+
+  @Query("""
+        SELECT c.* FROM customers c
+        WHERE (LOWER(c.name) || ' ' || LOWER(c.lastname))
+              LIKE LOWER(CONCAT('%', :q, '%'))
+      """)
+  Flux<Customer> searchByFullName(String q);
 
 }
