e11298842c6aa6592c5cebd63538bb4cf68cce48
diegoseg_15 <diegoferv@gmail.com>
2025-09-28 09:58:26 -0500

    :bug: Corregir AddressRepository agregando anotación @Repository

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/AddressRepository.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/AddressRepository.java
@@ -1,9 +1,12 @@
 package com.minegocio.MN_Data_Management.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.minegocio.MN_Data_Management.domain.Address;
 
+
+@Repository
 public interface AddressRepository extends ReactiveCrudRepository<Address, Long> {
-    
+
 }
