c2a17176487adf8989869995c9623ac0268fc33d
diegoseg_15 <diegoferv@gmail.com>
2025-09-28 14:19:23 -0500

    :sparkles: Añadir endpoint para obtener cliente por identificación

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/controllers/CustomersController.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/controllers/CustomersController.java
@@ -1,11 +1,11 @@
 package com.minegocio.MN_Data_Management.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ import com.minegocio.MN_Data_Management.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/v1/data")
@@ -39,6 +39,10 @@ public class CustomersController {
     }
 
     // Funcionalidad para buscar clientes por Identificación
+    @GetMapping("/clientes/identificacion/{identification}")
+    public Mono<Customer> getCustomerByIdentification(@PathVariable String identification) {
+        return customerService.getByIdentification(identification);
+    }
 
     // Funcionalidad para crear un nuevo cliente con la dirección matriz
 
