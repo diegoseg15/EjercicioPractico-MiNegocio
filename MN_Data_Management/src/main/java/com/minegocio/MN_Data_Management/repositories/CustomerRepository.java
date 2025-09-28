f83a2c6b962bcebec005e2c333b0baf2eb34598c
diegoseg_15 <diegoferv@gmail.com>
2025-09-28 10:01:02 -0500

    :sparkles: Añadir métodos de búsqueda y eliminación por companyId e identificación en CustomerRepository

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/CustomerRepository.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/CustomerRepository.java
@@ -3,14 +3,17 @@ package com.minegocio.MN_Data_Management.repositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
 import com.minegocio.MN_Data_Management.domain.Customer;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
-  @Query("SELECT c.* FROM customers c")
-  Flux<Customer> findAllProjected(); 
+
+  @Query("SELECT c.* FROM customers c WHERE c.company_id = :companyId AND identification = :identification")
+  Mono<Customer> findByCompanyAndIdentification(Long companyId, String identification);
 
   @Query("SELECT c.* FROM customers c WHERE c.identification = :identification")
   Flux<Customer> findByIdentification(String identification);
@@ -18,11 +21,10 @@ public interface CustomerRepository extends ReactiveCrudRepository<Customer, Lon
   @Query("SELECT c.* FROM customers c WHERE c.company_id = :companyId")
   Flux<Customer> findByCompanyId(Long companyId);
 
-  @Query("""
-        SELECT c.* FROM customers c
-        WHERE (LOWER(c.name) || ' ' || LOWER(c.lastname))
-              LIKE LOWER(CONCAT('%', :q, '%'))
-      """)
+  @Query(" SELECT c.* FROM customers c WHERE (LOWER(c.name) LOWER(c.lastname)) LIKE LOWER(CONCAT('%', :q, '%'))")
   Flux<Customer> searchByFullName(String q);
+
+  @Query("DELETE FROM customers WHERE company_id = :companyId AND identification = :identification;")
+  Mono<Customer> deleteByIdentificationAndCompanyId(Long companyId, String identification);
 
 }
