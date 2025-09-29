5924049caad07c2d4b0f78fa09cb6886c52a8f8e
diegoseg_15 <diegoferv@gmail.com>
2025-09-28 19:55:47 -0500

    :safety_vest: Añadir validaciones con @Valid y @NotNull en CustomersController

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/DTO/CustomerAddressesDTO.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/DTO/CustomerAddressesDTO.java
@@ -3,8 +3,15 @@ package com.minegocio.MN_Data_Management.DTO;
 import com.minegocio.MN_Data_Management.domain.Address;
 import com.minegocio.MN_Data_Management.domain.Customer;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 public class CustomerAddressesDTO {
+  @Valid
+  @NotNull(message = "Customer cannot be null")
   private Customer customer;
+  @Valid
+  @NotNull(message = "Address cannot be null")
   private Address address;
 
   public CustomerAddressesDTO() {
