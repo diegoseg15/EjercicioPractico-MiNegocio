16171735c4c56b72afbf7d91280e5aab7557393d
diegoseg_15 <diegoferv@gmail.com>
2025-09-28 19:47:12 -0500

    :sparkles: Añadir queries personalizados en AddressRepository para búsqueda e inserción

--- MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/AddressRepository.java
+++ MN_Data_Management/src/main/java/com/minegocio/MN_Data_Management/repositories/AddressRepository.java
@@ -1,12 +1,20 @@
 package com.minegocio.MN_Data_Management.repositories;
 
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.minegocio.MN_Data_Management.domain.Address;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface AddressRepository extends ReactiveCrudRepository<Address, Long> {
+    @Query("SELECT a.* FROM customer_addresses a JOIN customers c ON c.id = a.customer_id WHERE c.company_id = :companyId AND c.identification = :identification")
+    Flux<Address> findByCompanyAndIdentification(Long companyId, String identification);
 
+    @Query("INSERT INTO customer_addresses (customer_id, alias, street, city, province, country, zip, is_headquarters, created_at, updated_at) SELECT c.id, :#{#a.alias}, :#{#a.street}, :#{#a.city}, :#{#a.province}, :#{#a.country}, :#{#a.zip}, COALESCE(:#{#a.isHeadquarters}, false), now(), now() FROM customers c WHERE c.company_id = :companyId AND c.identification = :customerIdentification RETURNING *;")
+    Mono<Address> saveByCompanyAndIdentification(Long companyId, String customerIdentification, Address a);
 }
